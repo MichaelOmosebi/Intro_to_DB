b1d516a86ca87abd50025b78ef2b3a0bed63f429
Michael Omosebi <omosebimichael@live.com>
2025-06-29 00:37:48 +0100

    add task3 file

--- task_4.sql
+++ task_4.sql
@@ -3,4 +3,4 @@ USE alx_book_store;
 -- a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
 -- WITHOUT  using the DESCRIBE or EXPLAIN statements
 
-SHOW CREATE TABLE Books;
+SHOW COLUMNS FROM books;
