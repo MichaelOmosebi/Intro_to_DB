f1a09cf3f6b108c7bb41915c62569bddace0ce72
Michael Omosebi <omosebimichael@live.com>
2025-06-29 18:47:17 +0100

    update task4 file

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,14 @@
+-- Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
+
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+-- The name of the file should be task_4.sql
+-- All SQL keywords should be in uppercase
+
+
+
 USE alx_book_store;
 
--- a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
--- WITHOUT  using the DESCRIBE or EXPLAIN statements
-
-SHOW COLUMNS FROM books;
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'books';
